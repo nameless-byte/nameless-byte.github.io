8d310ead06870182fa703f265e0a757b7e7e7513
nameless-byte <57141323+nameless-byte@users.noreply.github.com>
2019-11-30 20:17:29 +0700

    commit

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,4 +1,4 @@
-import { Sine } from "./gsap-core.js";
+
 
 //particlesJS.load('particles-js', 'assets/scripts/particlesjs-config.json', function() {
     //console.log('callback - particles.js config loaded');
@@ -7,26 +7,33 @@ let paths = document.querySelectorAll(".sampletext path");
 for (let i = 0; i < paths.length; i++) {
     console.log(`Letter ${i} is ${paths[i].getTotalLength()}`);
 }
-let height = window.innerHeight*1.06;
+let height = window.innerHeight * 1.06;
 let width = window.innerWidth;
-const particles = 100;
+const particles = 200;
 for (var i = particles - 1; i >= 0; i--) {
   
     var pos = Math.floor(Math.random() * 100);
     var delay = Math.random() * 5;
-    var duration = (Math.random() * 1) + 1;
+    var duration = Math.random() * 2 + 5;
     
     let droplet = document.createElement("div");
     droplet.className = "drop";
     droplet.style.left = pos + "%";
-
-    gsap.to(droplet, {
+    let topr = Math.random() * 100;
+    droplet.style.width = Math.random() * 3 + "px";
+    droplet.style.height = Math.random() * 3 + "px";
+    let tl = gsap.timeline();
+    tl.fromTo(droplet, {y: height * topr / 100}, {
+        duration: duration  * (100 - topr) / 100,
+        y: height,
+        ease: "none",
+    }).fromTo(droplet, {y: 0}, {
         duration: duration,
         y: height,
-        delay: delay,
         repeat: -1,
-        ease: "none"
+        ease: "none",
     });
-    
+    gsap.ticker.fps(60);
+    console.log(topr);
     document.getElementById('particles-container').appendChild(droplet);
   }
